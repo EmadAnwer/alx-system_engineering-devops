7dc6d886c1213fb681a7b071066a48a11bba71d9
Emad Anwer <emadanwer.official@gmail.com>
2023-10-03 00:39:16 +0300

    test adv

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,19 @@
 # configure an Nginx
 
+package{ 'nginx':
+  ensure => 'installed',
+}
+
+service{ 'nginx':
+  ensure => 'running',
+  enable => 'true',
+}
+
 file_line{'custom HTTP header':
-  path => '/root/alx/month-0/alx-system_engineering-devops/0x0F-load_balancer/test',
+  path => '/etc/nginx/sites-available/default',
   line => '        add_header X-Served-By \"${::hostname}\";',
   after   => 'server_name _;', 
   match   => '^server_name _;',
   notify  => Service['nginx'],
-  }
+    require => Package['nginx'],
+}
