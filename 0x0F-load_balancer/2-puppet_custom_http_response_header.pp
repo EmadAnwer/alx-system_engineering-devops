eeadb1ca8035101afa823f953cbe090ee963dc1c
Emad Anwer <emadanwer.official@gmail.com>
2023-10-02 23:22:28 +0300

    adv

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,11 +13,9 @@ file{'/etc/nginx/sites-available/default':
   ensure => 'file',
   content => "server {
     listen 80 default_server;
-
+    listen [::]:80 default_server ipv6only=on;
     add_header X-Served-By ${hostname};
     root /var/www/html;
-    index index.html index.htm;
-    server_name localhost;
     error_page 404 /404.html;
     location / {
         try_files \$uri \$uri/ =404;
@@ -31,16 +29,3 @@ file{'/etc/nginx/sites-available/default':
   notify => Service['nginx'],
   replace => 'true',
 }
-
-file {'/var/www/html/index.html':
-  ensure => 'file',
-  content => 'Hello World!',
-  require => Package['nginx'],
-  notify => Service['nginx'],
-  }
-
-file {'/var/www/html/404.html':
-  ensure => 'file',
-  content => "Ceci n'est pas une page",
-  require => Package['nginx'],
-  notify => Service['nginx'],}
