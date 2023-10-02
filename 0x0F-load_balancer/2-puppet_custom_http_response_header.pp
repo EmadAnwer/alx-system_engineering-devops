7a5ef4b0a6523429f02f1dfb99dfa4c32525def0
Emad Anwer <emadanwer.official@gmail.com>
2023-10-02 23:50:16 +0300

    tt

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,10 +1,31 @@
-# add custom header to the request
+#configure servers
 
-file_line{
-  'add custom header to the request':
-    path  => '/etc/nginx/sites-available/default',
-    line  =>  "add_header X-Served-By ${hostname};",
-    match => '^server_name',
-    after => 'server_name',
-    notify => Service['nginx'],
+exec { 'install_nginx':
+command  => 'apt -y update; apt -y install nginx',
+provider => 'shell'
+}
+
+file { '/var/www/html/index.html':
+ensure  => 'present',
+content => 'Hello World!',
+require => Exec['install_nginx'],
+}
+
+file { '/var/www/html/404.html':
+ensure  => 'present',
+content => "Ceci n'est pas une page",
+require => Exec['install_nginx'],
+}
+
+
+
+file_line { 'add header':
+path  => '/etc/nginx/sites-enabled/default',
+match => 'server_name _;$',
+line  => "\tserver_name _;\n\tadd_header X-Served-By ${hostname};",
+}
+
+exec { 'run':
+command  => 'sudo service nginx restart',
+provider => shell,
 }
