ddef6c50aae8306a733ebbc42bfd53d36003d908
Emad Anwer <emadanwer.official@gmail.com>
2023-10-03 00:47:12 +0300

    zZZZzzzZZzzz

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,46 @@
 # configure an Nginx
 
-exec { 'update':
-  command  => 'sudo apt-get update',
-  provider => shell,
+package{ 'nginx':
+  ensure => 'installed',
 }
--> package {'nginx':
-  ensure => present,
+
+service{ 'nginx':
+  ensure => 'running',
+  enable => 'true',
 }
--> file_line { 'header line':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  line   => "	location / {
-  add_header X-Served-By ${hostname};",
-  match  => '^\tlocation / {',
+
+file{'/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => "server {
+    listen 80 default_server;
+    listen [::]:80 default_server ipv6only=on;
+    add_header X-Served-By ${hostname};
+    root /var/www/html;
+    index index.html index.htm;
+    server_name localhost;
+    error_page 404 /404.html;
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+  replace => 'true',
 }
--> exec { 'restart service':
-  command  => 'sudo service nginx restart',
-  provider => shell,
-}
+
+file {'/var/www/html/index.html':
+  ensure  => 'file',
+  content => 'Hello World!',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+  }
+
+file {'/var/www/html/404.html':
+  ensure  => 'file',
+  content => "Ceci n'est pas une page",
+  require => Package['nginx'],
+  notify  => Service['nginx'],}
