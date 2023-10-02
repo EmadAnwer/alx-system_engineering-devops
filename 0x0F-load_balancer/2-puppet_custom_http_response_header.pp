9af67ac3a2bd725887c9d713038b6fe429ca11f8
Emad Anwer <emadanwer.official@gmail.com>
2023-10-03 00:41:27 +0300

    test

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,10 +8,9 @@ service{ 'nginx':
   ensure => 'running',
   enable => 'true',
 }
-
 file_line{'custom HTTP header':
   path => '/etc/nginx/sites-available/default',
-  line => '        add_header X-Served-By \"${::hostname}\";',
+  line => "        add_header X-Served-By ${hostname};",
   after   => 'server_name _;', 
   match   => '^server_name _;',
   notify  => Service['nginx'],
