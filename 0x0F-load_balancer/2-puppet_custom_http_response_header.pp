6f0930a238afb8e13d5ad574257c418196803819
Emad Anwer <emadanwer.official@gmail.com>
2023-10-02 23:45:10 +0300

    t

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,46 +1,10 @@
-# configure an Nginx
+# add custom header to the request
 
-package{ 'nginx':
-  ensure => 'installed',
+file_line{
+  'add custom header to the request':
+    path  => '/etc/nginx/sites-available/default',
+    line  =>  "add_header X-Served-By ${hostname};",
+    match => '^server_name',
+    after => 'server_name',
+    notify => Service['nginx'],
 }
-
-service{ 'nginx':
-  ensure => 'running',
-  enable => 'true',
-}
-
-file{'/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => "server {
-    listen 80 default_server;
-    listen [::]:80 default_server ipv6only=on;
-    add_header X-Served-By ${hostname};
-    root /var/www/html;
-    index index.html index.htm;
-    server_name localhost;
-    error_page 404 /404.html;
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-}",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-  replace => 'true',
-}
-
-file {'/var/www/html/index.html':
-  ensure  => 'file',
-  content => 'Hello World!',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-  }
-
-file {'/var/www/html/404.html':
-  ensure  => 'file',
-  content => "Ceci n'est pas une page",
-  require => Package['nginx'],
-  notify  => Service['nginx'],}
