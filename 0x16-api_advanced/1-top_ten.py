08cced324625ae24cc9fb70d25ae0c3651558a3c
Emad Anwer <emadanwer.official@gmail.com>
2023-11-07 23:48:22 +0200

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ def top_ten(subreddit):
     """querie Reddit API and prints first 10 hot posts"""
 
     headers = {'User-Agent': 'MyPythonScript/1.0'}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=9"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     request = requests.get(url=url, headers=headers, allow_redirects=False)
     if request.status_code == 200:
         hot_ten = request.json().get('data').get('children')
