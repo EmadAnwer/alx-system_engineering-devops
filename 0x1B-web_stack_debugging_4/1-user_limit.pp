efa18c335450350861b294469eb3b8412ea43151
Emad Anwer <emadanwer.official@gmail.com>
2023-11-18 21:01:20 +0200

    fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # Increases the amount of file read
 
 exec {"fix-nginx",
-	command => 'sed -i "s/15/4096/" /etc/default/nginx ; sudo service nginx restart',
-	path    => '/usr/local/bin/:/bin/'
+    command => 'sed -i "s/15/4096/" /etc/default/nginx ; sudo service nginx restart',
+    path    => '/usr/local/bin/:/bin/'
 }
