2d5552354d4295953d1961d832529e541f6de5cd
Emad Anwer <emadanwer.official@gmail.com>
2023-11-18 23:46:11 +0200

    task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,12 @@
 # Increases the amount of file read
 
 exec { 'fix-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx ; service nginx restart',
-  path    => '/usr/local/bin/:/bin/',
+    command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+}
+
+# Restart nginx
+
+exec { 'nginx':
+  command => '/usr/sbin/service nginx restart',
+  require => Exec['fix-nginx'],
 }
